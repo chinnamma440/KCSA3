a6f06ca8f31d4587bbb2bb802e78c05f436b105a
DELL-1 <DELL-1@user>
2020-08-08 00:11:19 -1200

    test

---
+++ Actitime/src/test/java/com/code/Demo.java
@@ -0,0 +1,14 @@
+package com.code;
+
+import org.junit.Test;
+import org.testng.Reporter;
+
+public class Demo {
+
+	@Test
+	public void runTest() {
+		
+		Reporter.log("Successfully running", true);
+	}
+
+}
